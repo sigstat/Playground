ce72bb2dfad754afc283b546fa91fb6dd9106e78
Bence Kovari <kovari@aut.bme.hu>
2019-03-05 08:56:09 +0100

    Porting existing samples
    Porting WpfSamples and Console client

---
+++ SigStat.WpfSample/Helpers/DtwType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SigStat.WpfSample.Helpers
+{
+    public enum DtwType
+    {
+        NDtw,
+        FrameworkDtw,
+        MyDtw,
+        DtwPy,
+    }
+}
